a498b17641cc5f50d40e8f78b086293d64bfd2ab
yuta9999zn <yuta9999k@gmail.com>
2025-07-30 08:30:06 +0700

    update

--- Course/Microservices/Salon Booking/user-service/src/main/java/com/zosh/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zosh.repository;
-
-import com.zosh.modal.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
